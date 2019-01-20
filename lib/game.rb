8b1697b9e885fbc18b95d856e540c9dda41ede4b
topickering <topickering@gmail.com>
2019-01-20 16:42:54 +0000

    Game: added add_ship method

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,11 @@ class Game
 
   def initialize(board = Board.new)
     @board = board.set_up
+    @ships = [ ]
+  end
+
+  def add_ship(ship)
+    @ships << ship
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,11 +3,19 @@ require 'game'
 describe Game do
 
   let(:board) { double("Board") }
+  let(:frigate) { double("Ship", length: 2) }
+  let(:destroyer) { double("Ship", length: 3) }
 
   it "initializes with a board" do
     allow(board).to receive(:set_up) { board }
     test = Game.new(board)
     expect(test.board).to be board
+  end
+
+  describe "#add_ship" do
+    it 'adds a ship to the game' do
+      expect(subject.add_ship(frigate)).to eq [frigate]
+    end
   end
 
 end
